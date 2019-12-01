729f1043838c2a34ef4ff183cc2558bdc2b9f0fe
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2019-11-30 21:50:31 -0800

    solved medium with little help

---
+++ leetcode/p45.py
@@ -0,0 +1,39 @@
+'''
+@author: adnan
+Problem No. 102. Binary Tree Level Order Traversal (Medium)
+
+Runtime: 32 ms, faster than 92.82% of Python3 online submissions for Binary Tree Level Order Traversal.
+Memory Usage: 13.1 MB, less than 100.00% of Python3 online submissions for Binary Tree Level Order Traversal.
+'''
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, x):
+    self.val = x
+    self.left = None
+    self.right = None
+
+class Solution:
+  def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    if root == None:
+      return None
+    
+    ans = []
+    buffer = [root]
+    while buffer:
+      level = []
+      len_buffer = len(buffer)
+      print(buffer)
+      for i in range(len_buffer):
+        node = buffer.pop(0)
+        level.append(node.val)
+        if node.left != None: buffer.append(node.left)
+        if node.right != None: buffer.append(node.right)
+      ans.append(level)
+    return ans
+
+if __name__ == '__main__':
+  sol = Solution()
+  nums = [3,9,20,None,None,15,7]
+  ans = sol.levelOrder(root)
+  print(ans)
