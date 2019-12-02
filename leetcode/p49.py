3089aa205563135e4e588c9fb12d24ffdac25e6c
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2019-12-01 17:46:21 -0800

    Solved Medium

--- leetcode/p49.py
+++ leetcode/p49.py
@@ -1,27 +1,45 @@
 '''
 @author: adnan
-Problem No. 589. N-ary Tree Preorder Traversal (Easy)
+Problem No. 98. Validate Binary Search Tree (Medium)
 
-Runtime: 44 ms, faster than 99.21% of Python3 online submissions for N-ary Tree Preorder Traversal.
-Memory Usage: 14.5 MB, less than 100.00% of Python3 online submissions for N-ary Tree Preorder Traversal.
+Runtime: 40 ms, faster than 96.02% of Python3 online submissions for Validate Binary Search Tree.
+Memory Usage: 15.1 MB, less than 100.00% of Python3 online submissions for Validate Binary Search Tree.
 '''
 from typing import List
-# Definition for a Node.
-class Node:
-  def __init__(self, val=None, children=None):
-    self.val = val
-    self.children = children
+import tree_visualizer
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, x):
+    self.val = x
+    self.left = None
+    self.right = None
 
 class Solution:
-  def preorder(self, root: 'Node') -> List[List[int]]:
-    if root == None:
-      return []
-    buffer = [root]
-    ans = []
-    while buffer:
-      node = buffer.pop(0)
-      ans.append(node.val)
-      if node.children:
-        buffer = node.children + buffer
+  def isValidBST(self, root: TreeNode, Min=None, Max=None) -> bool:
+    if root:
+      if Min!=None and root.val <= Min:
+        return False
+      if Max!=None and root.val >= Max:
+        return False
 
-    return ans
+      if root.left and not(self.isValidBST(root.left, Min, root.val)):
+        return False
+      if root.right and not(self.isValidBST(root.right, root.val, Max)):
+        return False
+
+    return True
+
+if __name__ == '__main__':
+  sol = Solution()
+  root = tree_visualizer.deserialize('[5,1,4,null,null,3,6]')
+  ans = sol.isValidBST(root)
+  print(f'ans = {ans}')
+
+  root = tree_visualizer.deserialize('[2,1,3]')
+  ans = sol.isValidBST(root)
+  print(f'ans = {ans}')
+
+  root = tree_visualizer.deserialize('[1,null,1]')
+  ans = sol.isValidBST(root)
+  print(f'ans = {ans}')
+  tree_visualizer.drawtree(root)
