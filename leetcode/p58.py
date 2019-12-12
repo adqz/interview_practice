2b215a9a2d265311154f11593637272a23cdf53b
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2019-12-11 18:14:00 -0800

    Solved 2nd time with recursion - linked list easy

--- leetcode/p58.py
+++ leetcode/p58.py
@@ -2,8 +2,8 @@
 @author: adnan
 Problem 206. Reverse Linked List (Easy)
 
-Runtime: 40 ms, faster than 61.05% of Python3 online submissions for Reverse Linked List.
-Memory Usage: 14.6 MB, less than 38.64% of Python3 online submissions for Reverse Linked List.
+Runtime: 28 ms, faster than 97.39% of Python3 online submissions for Reverse Linked List.
+Memory Usage: 17.3 MB, less than 22.73% of Python3 online submissions for Reverse Linked List.
 '''
 from typing import List
 # Definition for singly-linked list.
@@ -12,6 +12,38 @@ class ListNode:
     self.val = x
     self.next = None
 
+class Solution:
+  def reverseList(self, head: ListNode) -> ListNode:
+    curr = head
+    if curr == None or curr.next == None: # base case
+      return curr
+  
+    p = self.reverseList(curr.next)
+    curr.next.next = curr
+    curr.next = None
+
+    return p
+
+  def make_linked_list(self, list_elemnts):
+    head = ListNode(list_elemnts[0])
+    node = head
+    for i in range(1, len(list_elemnts)):
+      new_node = ListNode(list_elemnts[i])
+      node.next = new_node
+      node = new_node
+    
+    return head
+
+if __name__ == '__main__':
+  sol = Solution()
+  head = sol.make_linked_list([7,9,8,2,3])
+  ans = sol.reverseList(head)
+  while ans:
+    print(ans.val, '-> ', end='')
+    ans = ans.next
+  print('Null')
+
+'''
 class Solution:
   def reverseList(self, head: ListNode) -> ListNode:
     if not head:
@@ -37,12 +69,4 @@ class Solution:
       node = new_node
     
     return head
-
-if __name__ == '__main__':
-  sol = Solution()
-  head = sol.make_linked_list([7,9,8,2,3])
-  ans = sol.reverseList(head)
-  while ans:
-    print(ans.val, '-> ', end='')
-    ans = ans.next
-  print('Null')
+'''
