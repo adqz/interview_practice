d986261f0ce97ad4a73ff051015582f04a41d5a9
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-18 10:11:52 -0800

    Array degree. Memory limit exceeded

--- leetcode/p71.py
+++ leetcode/p71.py
@@ -7,6 +7,51 @@ Time Limit exceeded. Try again
 from collections import Counter
 from typing import List
 class Solution:
+    def __init__(self):
+        self.cache = dict()
+    
+    def get_degree(self, arr):
+        arr = tuple(arr)
+        if arr in self.cache:
+            degree = self.cache[arr]
+        else:    
+            count = Counter(arr)
+            degree = max(count.values())
+            self.cache[arr] = degree
+        return degree
+
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        # 0. Corner cases
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return 1
+        # 1. Calculating degree of array
+        degree_nums = self.get_degree(nums)
+        # 2. Finding least length contiguous where degree(sub array) == degree(nums)
+        i, j = 0, len(nums)
+        min_len = float('inf')
+        while j>i: #revisit
+            flag = False
+            if self.get_degree(nums[i+1:j]) == degree_nums:
+                i += 1
+                flag = True
+            if self.get_degree(nums[i:j-1]) == degree_nums:
+                j -= 1
+                flag = True
+            if not flag:
+                break
+
+        return j-i
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.findShortestSubArray([1, 2, 2, 3, 1])) #2 
+    print(sol.findShortestSubArray([1,2,2,3,1,4,2])) #6
+
+'''
+class Solution:
+    # Brute Force
     def get_degree(self, arr):
         count = Counter(arr)
 
@@ -30,8 +75,4 @@ class Solution:
                         min_len = curr_len
         
         return min_len
-
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.findShortestSubArray([1, 2, 2, 3, 1])) #2 
-    print(sol.findShortestSubArray([1,2,2,3,1,4,2])) #6
+'''
