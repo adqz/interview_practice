f62fdb3423f55cd0d63e6b088950ff604fa8a2cc
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-18 10:23:54 -0800

    cleaned code

--- leetcode/p2.py
+++ leetcode/p2.py
@@ -1,19 +1,9 @@
 # @author: adnan
 # Problem No. 7
 class Solution(object):
-    '''
-    Problem: Given a 32-bit signed integer, reverse digits of an integer.
-    '''
     def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        assert isinstance(x, int)
-
         sign = 1 if x>0 else -1
         rev = ''
-
         for n in str(abs(x)):
             rev = n + rev
         rev = sign * int(rev)
