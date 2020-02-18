eca79795a1c1829fe6c3f73b5bd202d03dd49a9f
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-18 10:49:49 -0800

    cleaned code

--- leetcode/p12.py
+++ leetcode/p12.py
@@ -1,25 +1,11 @@
 # @author: adnan
 # 1003. Check If Word Is Valid After Substitutions (Medium)
 '''
-Runtime: 128 ms, faster than 21.92% of Python online submissions for Check If Word Is Valid After Substitutions.
+Runtime: 128 ms, faster than 29.23% of Python online submissions for Check If Word Is Valid After Substitutions.
 Memory Usage: 12.1 MB, less than 100.00% of Python online submissions for Check If Word Is Valid After Substitutions.
 '''
 class Solution(object):
     def isValid(self, S):
-        """
-        We are given that the string "abc" is valid.
-        From any valid string V, we may split V into two pieces X and Y such that X + Y 
-        (X concatenated with Y) is equal to V.  (X or Y may be empty.)  Then, X + "abc" + Y is also valid.
-        
-        If for example S = "abc", then examples of valid strings are: 
-        "abc", "aabcbc", "abcabc", "abcabcababcc".  
-        Examples of invalid strings are: "abccba", "ab", "cababc", "bac".
-        
-        Return true if and only if the given string S is valid
-
-        :type S: str
-        :rtype: bool
-        """
         stack = []
         for char in S:
           stack.append(char)
