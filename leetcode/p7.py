0f02b34f4bfc7f236f2f92271aa8be92b1b13fd8
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-20 06:28:19 -0800

    Solved longest non repeating substring. Medium

--- leetcode/p7.py
+++ leetcode/p7.py
@@ -1,43 +1,49 @@
-# ----------------------- UNSOLVED ----------------------- 986 / 987 test cases passed
-# @author: adnan
-# Problem 3. Longest Substring Without Repeating Characters
+'''
+@author: adnan
+Problem 3. Longest Substring Without Repeating Characters
+
+Runtime: 784 ms, faster than 7.22% of Python online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 12 MB, less than 67.97% of Python online submissions for Longest Substring Without Repeating Characters.
+'''
 class Solution(object):
 	def lengthOfLongestSubstring(self, s):
-		"""
-		:type s: str
-		:rtype: int
-		"""
-		# assert isinstance(s, str) and len(s) >= 0
-		# return 0 if s == ''
-		if s=='':
+		# 0. Corner case
+		if not s:
 			return 0
-		oldSubstring = ''
-		longestSubstring = s[0]
-		i = 0
-		while (i != len(s)-1):
-			# print(i)
-			newSubstring = ''
-			for j in range(i, len(s)):
-				if (s[j] in newSubstring):
-					break
-				else:
-					newSubstring += s[j]
-					# print(newSubstring)
-			if len(newSubstring) > len(oldSubstring):
-				longestSubstring = newSubstring
-				oldSubstring = newSubstring
-			i = j
+		if len(s) == 1:
+			return 1
+		
+		# 1. General case
+		pointer_fixed, pointer_moving = 0, 0
+		seen = set()
+		longest_str_len = 1
+		
+		while pointer_moving < len(s):
+			# If character is unique
+			if s[pointer_moving] not in seen:
+				seen.add(s[pointer_moving])
+				curr_len = pointer_moving - pointer_fixed + 1 #length of substring
+				if curr_len > longest_str_len:
+					longest_str_len = curr_len
+				pointer_moving += 1 #moving pointer ahead
+			else:
+				pointer_fixed += 1
+				pointer_moving = pointer_fixed
+				seen = set()
+		
+		return longest_str_len
 
-		return len(longestSubstring)
-
+				
 if __name__ == '__main__':
   # ------ Test Cases ------
   s = Solution()
-  print(s.lengthOfLongestSubstring('abc'))
-  print(s.lengthOfLongestSubstring('abca'))
-  print(s.lengthOfLongestSubstring('abbbccc'))
-  print(s.lengthOfLongestSubstring('aabcdefa'))
-  print(s.lengthOfLongestSubstring(''))
+  print(s.lengthOfLongestSubstring('abc')) #3
+  print(s.lengthOfLongestSubstring('abcabcb')) #3
+  print(s.lengthOfLongestSubstring('bbbbb')) #1
+  print(s.lengthOfLongestSubstring('pwwkew')) #3
+  print(s.lengthOfLongestSubstring('abbbccc')) #2
+  print(s.lengthOfLongestSubstring('aabcdefa')) #6
+  print(s.lengthOfLongestSubstring('')) #0
 
 
 
