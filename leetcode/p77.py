81bd6b8239fee8e8f090438a12efd9287f9fe820
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-26 12:08:25 -0800

    Solved find right interval. Medium. Used binary search

--- leetcode/p77.py
+++ leetcode/p77.py
@@ -2,6 +2,8 @@
 @author: adnan
 Problem 436. Find Right Interval (Medium)
 
+Runtime: 436 ms, faster than 20.78% of Python3 online submissions for Find Right Interval.
+Memory Usage: 18.8 MB, less than 33.33% of Python3 online submissions for Find Right Interval.
 '''
 from typing import List
 class Solution:
@@ -10,27 +12,44 @@ class Solution:
         if len(intervals) <= 1:
             return [-1]
         ans = []
-        for start_i, end_i in intervals:
-            min_start_j = float('inf')
-            min_start_j_index = None
-            # Going through each interval
-            for j, (start_j, end_j) in enumerate(intervals):
-                if start_j >= end_i:
-                    # Finding min start_j which is greater than end_i
-                    if start_j < min_start_j:
-                        min_start_j = start_j
-                        min_start_j_index = j
-            if min_start_j != float('inf'):
-                ans.append(min_start_j_index)
-            else:
+        interval_indices = {tuple(interval): i for i, interval in enumerate(intervals)}
+        # Sort according to interval start point - O(nlog(n))
+        intervals_sorted = sorted(intervals, key = lambda interval: interval[0])
+        start_points = sorted([interval[0] for interval in intervals])
+        for start, end in intervals:
+            i = self.binary_search(start_points, target = end)
+            if i == -1:
                 ans.append(-1)
-        
+            else:
+                index = interval_indices[tuple(intervals_sorted[i])]
+                ans.append(index)
         return ans
+    
+    def binary_search(self, arr, target):
+        if not arr:
+            return -1
+        l, r = 0, len(arr) - 1
+        while l+1 < r:
+            mid = l + (r-l)//2
+            if arr[mid] == target:
+                r = mid
+                # return mid
+            if arr[mid] < target:
+                l = mid
+            else:
+                r = mid
+        # Post Processing
+        if arr[l] == target: return l
+        if arr[r] == target: return r
+        if arr[r] > target: return r
+
+        return -1
 
 
 if __name__ == "__main__":
     sol = Solution()
     print(sol.findRightInterval([ [3,4], [2,3], [1,2] ])) # [-1, 0, 1]
+    print(sol.findRightInterval([ [11,15], [1,3], [2,9] ])) # [-1, 0, 0]
     print(sol.findRightInterval([ [1,2] ])) # [-1]
     print(sol.findRightInterval([ [1,4], [2,3], [3,4] ])) # [-1, 2, -1]
     print(sol.findRightInterval([ [4,5],[2,3],[1,2] ])) # [-1,0,1]
