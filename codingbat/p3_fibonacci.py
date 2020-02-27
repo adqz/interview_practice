1fac1a4e3cbdacfc3af348b65512a61e3cc8d986
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-02-27 15:07:42 -0800

    Solved fib. Easy

--- codingbat/p3_fibonacci.py
+++ codingbat/p3_fibonacci.py
@@ -1,12 +1,16 @@
-def fibonacci(n, cache=dict()):
-    if n in cache:
-        return cache[n]
-    if n in [0,1]:
-        return 1
-    else:
-        ans = fibonacci(n-1) + fibonacci(n-2)
-        cache[n] = ans
-        return ans
+class Solution:
+    def fibonacci(self, n, cache=dict()):
+        if n in cache:
+            return cache[n]
+        if n in [0,1]:
+            return 1
+        else:
+            ans = self.fibonacci(n-1) + self.fibonacci(n-2)
+            cache[n] = ans
+            return ans
 
 if __name__ == "__main__":
-    print(fibonacci(40))
+    sol = Solution()
+    print(sol.fibonacci(0))
+    print(sol.fibonacci(1))
+    print(sol.fibonacci(10))
