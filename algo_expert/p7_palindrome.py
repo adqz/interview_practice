3c4157c5141e7110005fef36d1e54d9b15f3e7c9
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-04-08 23:15:01 -0700

    solved palindrome

---
+++ algo_expert/p7_palindrome.py
@@ -0,0 +1,12 @@
+def isPalindrome(string):
+    l, r = 0, len(string) - 1
+    while l < r:
+        if string[l] != string[r]:
+            return False
+        l += 1
+        r -= 1
+    return True
+
+if __name__ == "__main__":
+    print(isPalindrome('adnan')) #False
+    print(isPalindrome('madam')) #True
