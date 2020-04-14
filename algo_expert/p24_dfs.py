1686bc3d9dced3197fc533f46d2470c49d1f8396
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-04-14 13:34:55 -0700

    solved dfs

---
+++ algo_expert/p24_dfs.py
@@ -0,0 +1,32 @@
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        
+        def helper_dfs(node, visited, array):
+            if node is None:
+                return
+            
+            array.append(node.name)
+            visited.add(node)
+
+            for child in node.children:
+                if child not in visited:
+                    helper_dfs(child, visited, array)
+            return
+        
+        visited = set()
+        array = []
+        helper_dfs(self, visited, array)
+        return array
+                
