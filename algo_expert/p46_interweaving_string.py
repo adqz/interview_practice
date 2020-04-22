b3329f57ad160221da22690e122ff96492b1d63c
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-04-21 19:38:27 -0700

    solved interweaving strings

--- algo_expert/p46_interweaving_string.py
+++ algo_expert/p46_interweaving_string.py
@@ -2,19 +2,24 @@ def interweavingStrings(one, two, three):
     # Preliminary check
     if len(one) + len(two) != len(three):
         return False
+
+    return helper_interweavingStrings(one, two, three, 0, 0)
     
-    i, j = 0, 0
-    for k in range(len(three)):
-        if i < len(one) and three[k] == one[i]:
-            i += 1
-        elif j < len(two) and three[k] == two[j]:
-            j += 1
-        else:
-            return False
-    if (i != len(one)) or (j != len(two)):
-        return False
+def helper_interweavingStrings(one, two, three, i, j):
+    k = i+j
+    # Base case
+    if k == len(three):
+        return True
     
-    return True
+    if i < len(one) and one[i] == three[k]:
+        if helper_interweavingStrings(one, two, three, i+1, j):
+            return True
+    
+    if j < len(two) and two[j] == three[k]:
+        if helper_interweavingStrings(one, two, three, i, j+1):
+            return True
+
+    return False
 
 if __name__ == "__main__":
     one   = 'aabcc'
