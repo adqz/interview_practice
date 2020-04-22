8bbb131d2fad8c2cec732cac95c577415758c37d
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-04-22 13:43:31 -0700

    code clean up

--- algo_expert/p56_delete_zero_sum_subtrees.py
+++ algo_expert/p56_delete_zero_sum_subtrees.py
@@ -1,8 +1,10 @@
 def delete_zero_sum_subtree(root):
-    post_order_traverse_dfs(root, None, dir='None')
+    root_sum = post_order_traverse_dfs(root)
+    if root_sum == 0:
+        root == None
     return root
 
-def post_order_traverse_dfs(node, parent, dir):
+def post_order_traverse_dfs(node):
     if node is None:
         return node
     # Base
@@ -10,20 +12,15 @@ def post_order_traverse_dfs(node, parent, dir):
         return node.data
 
     # Build
-    value_left, value_right = 0, 0
-    if node.left:
-        value_left = post_order_traverse_dfs(node.left, node, dir='left')
-    if node.right:
-        value_right = post_order_traverse_dfs(node.right, node, dir='right')
+    sum_left = post_order_traverse_dfs(node.left)
+    sum_right = post_order_traverse_dfs(node.right)
     
-    subtree_sum = node.data + value_left + value_right
-    if subtree_sum == 0:
-        if dir == 'left':
-            parent.left = None
-        elif dir == 'right':
-            parent.right = None
+    if sum_left == 0:
+        node.left = None
+    if sum_right == 0:
+        node.right = None
     
-    return subtree_sum
+    return node.data + sum_left + sum_right
 
 def is_leaf(node):
     if node.left is None and node.right is None:
