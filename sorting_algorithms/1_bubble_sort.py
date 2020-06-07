8042e038dac0a76a60da21a9eaf97383f527a60d
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-06 17:43:30 -0700

    updated unit tests

--- sorting_algorithms/1_bubble_sort.py
+++ sorting_algorithms/1_bubble_sort.py
@@ -1,5 +1,13 @@
+import unittest
+
 # O(N^2) time | O(1) space
 def bubbleSort(array):
+    '''
+    Time Complexity
+    Best: O(n) - if array is already sorted
+    Avg: O(N^2)
+    Worst: O(N^2)
+    '''
     if len(array) in [0, 1]:
         return array
     isSorted = False
@@ -17,9 +25,42 @@ def bubbleSort(array):
 def swap(array, i, j):
     array[i], array[j] = array[j], array[i]
 
+class TestStringMethods(unittest.TestCase):
+    def setUp(self): 
+        self.testCases = [  [], 
+                            [2], 
+                            [1000,1,6], 
+                            [5,1,4,2,3],
+                        ]
+
+        self.answers = [    [],
+                            [2],
+                            [1, 6, 1000],
+                            [1, 2, 3, 4, 5],
+                        ]
+
+    def test_1(self):
+        '''
+        Edge case 1: Empty array
+        '''
+        ans = bubbleSort(self.testCases[0])
+        self.assertEqual(ans, self.answers[0])
+    
+    def test_2(self):
+        '''
+        Edge case 2: Single element array
+        '''
+        ans = bubbleSort(self.testCases[1])
+        self.assertEqual(ans, self.answers[1])
+    
+    def test_3(self):
+        '''
+        General case 3 and 4
+        '''
+        for i in range(2,4):
+            ans = bubbleSort(self.testCases[i])
+            self.assertEqual(ans, self.answers[i])
+
 if __name__ == "__main__":
-    print(bubbleSort([])) # []
-    print(bubbleSort([2])) # [2]
-    print(bubbleSort([1000,1,6])) # [1, 6, 1000]
-    print(bubbleSort([5,1,4,2,3])) # [1, 2, 3, 4, 5]
+    unittest.main()
     
