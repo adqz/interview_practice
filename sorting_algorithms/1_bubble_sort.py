cc6617cf9a6788b1c8c693d42d706041d66be8e4
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-06 17:09:28 -0700

    revised bubble sort

---
+++ sorting_algorithms/1_bubble_sort.py
@@ -0,0 +1,18 @@
+# O(N^2) time | O(1) space
+def bubbleSort(array):
+    isSorted = False
+    counter = 0
+
+    while not isSorted:
+        isSorted = True
+        for i in range(0, len(array) - 1 - counter):
+            if array[i] > array [i+1]:
+                swap(array, i, i+1)
+                isSorted = False
+        counter += 1
+    return array
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+
+if __name__ == "__main__":
